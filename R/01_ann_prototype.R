5fac4acbfffa20e1f5d13aa95520245d434df765
Leon Jessen <leon.jessen@gmail.com>
2020-10-02 18:56:38 +0000

    Update prototype with accuracy

--- R/01_ann_prototype.R
+++ R/01_ann_prototype.R
@@ -1,58 +1,61 @@
-# Clear workspace to avoid unintentional reuse of old variables
-# ------------------------------------------------------------------------------
-rm(list = ls())
-
-# Then we define the functions needed to implement the ANN
-# ------------------------------------------------------------------------------
+# Define the functions needed to implement the ANN ------------------------
 
 # Dot product between two vectors
 # a . b = a1b1 + a2b2 + ... + anbn
-dot_prod = function(a, b){
+dot_prod <- function(a, b){
   stopifnot( is.numeric(a) & is.numeric(b) & length(a) == length(b) )
-  ab = 0
+  ab <- 0
   for( i in seq(from = 1, to = length(a)) ){
-    ab = ab + (a[i] * b[i])
+    ab <- ab + (a[i] * b[i])
   }
   return(ab)
 }
 
 # Matrix multiplication
 # The dots products of the i'th row of A and the j'th column of B
-mat_mult = function(A, B){
+mat_mult <- function(A, B){
   stopifnot( is.numeric(A) & is.numeric(B) & ncol(A) == nrow(B) )
-  AB = matrix(nrow = nrow(A), ncol = ncol(B))
+  AB <- matrix(nrow = nrow(A), ncol = ncol(B))
   for( i in 1:nrow(A) ){
     for( j in 1:ncol(B) ){
-      AB[i,j] = dot_prod(A[i,], B[,j])
+      AB[i,j] <- dot_prod(A[i,], B[,j])
     }
   }
   return(AB)
 }
 
 # Sigmoid activation function
-# The function carrying the non-linearity of an ANN
-s = function(x){ 1 / (1 + exp(-x)) }
+# The function defining the non-linearity of an ANN
+s <- function(x){ 1 / (1 + exp(-x)) }
 
 # Mean squared error function
 # Used to measure the difference between what the ANN predicts (O) and the
 # target value (t)
-mse = function(O, t){
+mse <- function(O, t){
   return( 1/2 * (O - t) ** 2 )
+}
+
+# Accuracy is a measure of prediction quality
+# Out of all the predictions, what percentage were correct?
+accuracy <- function(y_pred, y_true, thres = 0.5){
+  y_pred <- as.numeric(y_pred >= 0.5)
+  acc <- sum(y_pred == y_true) / length(y_pred)
+  return( acc )
 }
 
 # Feed forward algorithm
 # The process transforming the input vector to the prediction of the ANN
-feed_forward = function(x, v, w){
-  x = matrix(c(x, 1)) # Add bias neuron
-  h = rbind(mat_mult(v, x), 1) # Add bias neuron
-  H = s(h)
-  o = mat_mult(w, H)[1,1]
-  O = s(o)
+feed_forward <- function(x, v, w){
+  x <- matrix(c(x, 1)) # Add bias neuron
+  h <- rbind(mat_mult(v, x), 1) # Add bias neuron
+  H <- s(h)
+  o <- mat_mult(w, H)[1,1]
+  O <- s(o)
   return(O)
 }
 
-# Prepare the data to work on. This is R, so naturally we will be using iris
-# ------------------------------------------------------------------------------
+
+# Prepare the data --------------------------------------------------------
 
 # Take a moment to look at the data
 head(iris)
@@ -63,13 +66,13 @@ dim(iris)
 # and 3 flower species labels: setosa, versicolor, virginica (This is our y)
 #
 # We are going to model setosa yes/no, so we will prepare the data as follows:
-X = iris[,-5] # Remove the labels to get our X
-colnames(X) = paste0('x', seq(1, ncol(X))) # Create some nice column names
-X = apply(X, 2, function(x){ return( as.numeric(scale(x)) ) }) # Scale obs
-y = ifelse(iris$Species == 'setosa', 1, 0) # Set the label setosa yes/no
-i = sample(seq(1, length(y))) # shuffle the rows of the data
-X = X[i,] # Apply shuffling
-y = y[i] # Apply shuffling
+X <- iris[,-5] # Remove the labels to get our X
+colnames(X) <- paste0('x', seq(1, ncol(X))) # Create some nice column names
+X <- apply(X, 2, function(x){ return( as.numeric(scale(x)) ) }) # Scale obs
+y <- ifelse(iris$Species == 'setosa', 1, 0) # Set the label setosa yes/no
+i <- sample(seq(1, length(y))) # shuffle the rows of the data
+X <- X[i,] # Apply shuffling
+y <- y[i] # Apply shuffling
 
 # The prepared data looks like so:
 dim(X)
@@ -81,103 +84,117 @@ head(y)
 # x1, x2, x3 and x4 and correctly assigns setosa yes/no, i.e. a 1 if it indeed
 # setosa and a 0 if it is not, so binary classification
 
-# Now we are ready to train the network
-# ------------------------------------------------------------------------------
+
+# Now we are ready to train the network -----------------------------------
 
 # We start by setting the hyperparameters:
-n_hidden = 1    # Number of hidden neurons (Only 1 hidden layer here)
-epochs   = 5    # Number of epochs (Number of passes of entire data set)
-epsilon  = 1000 # Learning rate (Step size, when adjusting the weights)
+n_hidden <- 1   # Number of hidden neurons (Only 1 hidden layer here)
+epochs <- 5     # Number of epochs (Number of passes of entire data set)
+epsilon <- 1000 # Learning rate (Step size, when adjusting the weights)
 
 # And defining and initialising the the needed weight matrices
 
 # Set dimensions for weight matrices
-v_n_row = n_hidden     # to hidden layer
-v_n_col = ncol(X) + 1  # From input layer
-w_n_row = 1            # to output layer
-w_n_col = n_hidden + 1 # From hidden layer
+v_n_row <- n_hidden     # to hidden layer
+v_n_col <- ncol(X) + 1  # From input layer
+w_n_row <- 1            # to output layer
+w_n_col <- n_hidden + 1 # From hidden layer
 
 # Initialise weight matrices
 # Note how weight are initialised randomly
-v = matrix(data = rnorm(v_n_row * v_n_col), nrow = v_n_row, ncol = v_n_col)
-w = matrix(data = rnorm(w_n_row * w_n_col), nrow = w_n_row, ncol = w_n_col)
+v <- matrix(data = rnorm(v_n_row * v_n_col), nrow = v_n_row, ncol = v_n_col)
+w <- matrix(data = rnorm(w_n_row * w_n_col), nrow = w_n_row, ncol = w_n_col)
 
 
-# Now we are finally ready to run the training
-# ------------------------------------------------------------------------------
+# Now we are finally ready to run the training ----------------------------
+
 # Run back propagation
-training = matrix(NA, nrow = epochs, ncol = 2)
+training <- matrix(NA, nrow = epochs, ncol = 3,
+                   dimnames = list(seq(1, epochs), c("epoch", "mse", "acc")))
 for( l in seq(1, epochs) ){
   
-  errors = rep(NA, nrow(X))
+  errors <- rep(NA, nrow(X))
+  preds <- rep(NA, nrow(X))
   
   for( i in seq(1, nrow(X)) ){
+    
     # Feed Forward
-    x_i = matrix(c(X[i,], 1)) # Add bias neuron
-    h   = rbind(mat_mult(v, x_i), 1) # Add bias neuron
-    H   = s(h)
-    o   = mat_mult(w, H)[1,1]
-    O   = s(o)
+    x_i <- matrix(c(X[i,], 1))      # Add bias neuron
+    h <- rbind(mat_mult(v, x_i), 1) # Add bias neuron
+    H <- s(h)
+    o <- mat_mult(w, H)[1,1]
+    O <- s(o)
     
     # Calculate error of current prediction
-    y_i = y[i]
-    err = mse(O, y_i)
-    errors[i] = err
+    y_i <- y[i]
+    err <- mse(O, y_i)
+    errors[i] <- err
+    
+    # Save the current prediction, i.e. O
+    preds[i] = O
     
     # Calculate value of delta function
-    s_prime_o = (1 - O) * O
-    delta = (O - y_i) * s_prime_o
+    s_prime_o <- (1 - O) * O
+    delta <- (O - y_i) * s_prime_o
     
     # Compute w-weights changes
-    dE_dw = t(delta * H)
+    dE_dw <- t(delta * H)
     
     # Compute v-weights changes
-    s_prime_h = (1 - H) * H
-    dE_dv = matrix(data = NA, nrow = nrow(v), ncol = ncol(v))
+    s_prime_h <- (1 - H) * H
+    dE_dv <- matrix(data = NA, nrow = nrow(v), ncol = ncol(v))
     
     for( j in 1:nrow(dE_dv) ){
       
       for( k in 1:ncol(dE_dv) ){
-        dE_dv[j,k] = delta * w[1,j] * s_prime_h[j,1] * x_i[k,1]
+        dE_dv[j,k] <- delta * w[1,j] * s_prime_h[j,1] * x_i[k,1]
       }
     }
     
     # Update weight matrices
-    w = w - epsilon * dE_dw
-    v = v - epsilon * dE_dv
+    w <- w - epsilon * dE_dw
+    v <- v - epsilon * dE_dv
     
   }
   
   # Calculate mean errors for current epoch
-  training[l,] = c(l, mean(errors))
+  training[l,] <- c(l, mean(errors), accuracy(y_pred = preds, y_true = y))
   
 }
 
 # With the model we trained, we can predict on the training data and compare
 # with the target values:
 
-# Run predictions
-# ------------------------------------------------------------------------------
-y_true = y
-y_pred = rep(NA, nrow(X))
+
+# Run predictions ---------------------------------------------------------
+y_true <- y
+y_pred <- rep(NA, nrow(X))
 for( i in seq(1, nrow(X)) ){
-  y_pred[i] = feed_forward(X[i,], v, w)
+  y_pred[i] <- feed_forward(X[i,], v, w)
 }
 
-# Calculate accuracy
-# ------------------------------------------------------------------------------
-thres = 0.5
-y_pred_bin = (y_pred > thres) + 0
-acc = sum(y_true == y_pred_bin) / length(y_true)
+
+# Calculate accuracy ------------------------------------------------------
+thres <- 0.5
+acc <- accuracy(y_pred, y_true, thres)
 cat("Using ", n_hidden, " hidden neurons, a learning rate of ", epsilon,
     " and training for ", epochs, " epochs, you got an accuracy of ",
     round(acc, 3), "\n", sep = "")
 
 # Lastly, we can visualise the training and the final performance of the model:
 
-# Plot results
-# ------------------------------------------------------------------------------
-par(mfrow=c(1,2))
-plot(training, type = "l", xlab="epoch", ylab = "MSE")
-plot(cbind(y_pred, y_true))
-abline(v = thres, lty = 2)
+# Plot results ------------------------------------------------------------
+par(mfrow = c(1, 3))
+plot(training[,"epoch"], training[,"mse"], type = "l",
+     xlab = "epoch",
+     ylab = "mean-squared-error",
+     main = "Error as a function of training rounds")
+plot(training[,"epoch"], training[,"acc"], type = "l",
+     xlab = "epoch",
+     ylab = "accuracy",
+     main = "Accuracy as a function of training rounds")
+plot(cbind(y_pred, y_true),
+     xlab = "y_pred (predicted value)",
+     ylab = "y_true (actual value)",
+     main = "True values versus predicted values")
+abline(v = thres, lty = 2) # Display threshold for defining pos / negs
